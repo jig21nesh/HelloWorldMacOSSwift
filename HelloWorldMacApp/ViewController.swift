067814bd89fa71b435e8160e500379a4e8aeb5d3
jigneshkakkad <jig2nesh@gmail.com>
2016-05-27 15:31:23 +1000

    Initial commit

---
+++ HelloWorldMacApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloWorldMacApp
+//
+//  Created by Jignesh Kakkad on 27/05/2016.
+//  Copyright © 2016 WeInnovateIT. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
